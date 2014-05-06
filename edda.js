838c966e8791e34e924912b1393bc09ba60a805a
Max Irwin <max_irwin@yahoo.com>
2014-05-05 21:25:10 -0400

    Placeholder

---
+++ edda.js
@@ -0,0 +1,15 @@
+/****************************************************************************
+*
+* Edda - a Documentation generator for Heimdall APIs 
+* (c)Copyright 2014, Max Irwin
+* MIT License
+*
+****************************************************************************/
+
+var jade = require('jade');
+
+var Edda = module.exports = {};
+
+var generate = Edda.generate = function(api) {
+	return {};
+};
